6c8d739fa8bc7f1fb7c86b1df8d02e00f630f9f4
FredrikPedersen <fredrikhp@gmail.com>
2019-10-07 22:59:05 +0200

    Completed Section 3: Part 48: Working with Inline Styles

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -30,10 +30,22 @@ class App extends Component {
     };
 
     render() {
+
+        const style = {
+          backgroundColor: 'white',
+          font: 'inherit',
+          border: '1px solid blue',
+          padding: '8px',
+          cursor: 'pointer'
+        };
+
         return(
             <div className="App">
                 <h1>Hi, I'm a React App</h1>
-                <button onClick={() => this.switchNameHandler('Fredrikke')}>Switch Names</button>
+                <button
+                    onClick={() => this.switchNameHandler('Fredrikke')}
+                    style={style}>
+                    Switch Names</button>
                 <Person
                     name={this.state.persons[0].name}
                     age={this.state.persons[0].age}
