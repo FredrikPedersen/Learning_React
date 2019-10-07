ec1f44161a8aca6778a374b4c350d0839df61a37
FredrikPedersen <fredrikhp@gmail.com>
2019-10-07 21:38:52 +0200

    Completed Setion 3: Part 38

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -4,15 +4,23 @@ import Person from './Person/Person'
 
 
 class App extends Component{
-  render() {
-    return (
-        <div className="App">
-          <h1>Hi, I'm a React App</h1>
-          <p>This is really working!</p>
-          <Person name="Fredrik" age="25">My Hobbies: React</Person>
-        </div>
-    );
-  }
+    state = {
+        persons: [
+            { name: 'Fredrik', age: 25 },
+            { name: "Anders", age: 23}
+        ]
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hi, I'm a React App</h1>
+                <button>Switch Name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>My Hobbies: React</Person>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 const person = (props) => {
     return (
-        <p>My name is {props.name}, and I am {props.age} years old!</p>
+        <div>
+            <p>My name is {props.name}, and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+        </div>
     );
 };
 
