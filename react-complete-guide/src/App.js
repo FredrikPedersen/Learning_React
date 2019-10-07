af9a818f02e17d83704c66009c935cc7f3a253be
FredrikPedersen <fredrikhp@gmail.com>
2019-10-07 20:58:21 +0200

    Finnished Section 3: Part 29

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -3,11 +3,7 @@ import './App.css';
 
 class App extends Component{
   render() {
-    return (
-        <div className="App">
-          <h1>Hi, I'm a React App</h1>
-        </div>
-    );
+    return React.createElement('div', null, React.createElement('h1', {className: 'App'}, 'Hi, I\'m a React App!'));
   }
 }
 
