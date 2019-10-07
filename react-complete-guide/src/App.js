07d991b5a76279d18fd8db32e694c1874c3178b6
FredrikPedersen <fredrikhp@gmail.com>
2019-10-07 20:40:53 +0200

    Completed Section 3: Part 27

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,14 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello There</h1>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+    </div>
+  );
 }
 
 export default App;
