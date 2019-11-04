22f61a7b42e3121cab1ad577b3fe14083664ecbf
FredrikPedersen <fredrikhp@gmail.com>
2019-11-04 18:55:30 +0100

    Completed Section 7: Part 104: Using React.Fragment

--- react-complete-guide/src/Components/Persons/Person/Person.js
+++ react-complete-guide/src/Components/Persons/Person/Person.js
@@ -1,16 +1,14 @@
 import React, {Component} from "react";
-import classes from "./Person.css";
-
 
 class Person extends Component {
     render() {
         console.log("[Person.js rendering...");
         return (
-            <div className={classes.Person}>
-                <p onClick={this.props.click}>My name is {this.props.name}, and I am {this.props.age} years old!</p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.change} value={this.props.name}/>
-            </div>
+            <>
+                <p key="i1" onClick={this.props.click}>My name is {this.props.name}, and I am {this.props.age} years old!</p>
+                <p key="i2">{this.props.children}</p>
+                <input key="i3" type="text" onChange={this.props.change} value={this.props.name}/>
+            </>
         );
     }
 }
