1fe39f2e6828828fab770136b94dd016ad086769
FredrikPedersen <fredrikhp@gmail.com>
2019-11-04 17:15:38 +0100

    Completed Section 7: Part 94: Controlling the useEffect() Behavior

--- react-complete-guide/src/Components/Cockpit/Cockpit.js
+++ react-complete-guide/src/Components/Cockpit/Cockpit.js
@@ -5,8 +5,14 @@ const cockpit = (props) => {
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useEffect(() => {
-        console.log("[Cockpit.js] useEffect")
-    });
+        console.log("[Cockpit.js] useEffect");
+
+        //Imitate a Http Request
+        setTimeout(() => {
+            alert("Data saved to cloud!")
+        }, 1000);
+    }, []); //Only triggers once the app is rendered. Add values (like props.persons) inside the array to trigger useEffect() every time a change is made to that value. May take in several values.
+    //You may have more than one useEffect setups, so it can be triggered by different changes!
 
     const assignedClasses = [];
     let btnClass = "";
