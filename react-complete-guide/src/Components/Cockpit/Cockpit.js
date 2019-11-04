d6d3a2497f234eb476cbe6ee931d9170b66b0601
FredrikPedersen <fredrikhp@gmail.com>
2019-11-04 20:07:32 +0100

    Completed Section 7: Part 110: Refs with React Hooks

--- react-complete-guide/src/Components/Cockpit/Cockpit.js
+++ react-complete-guide/src/Components/Cockpit/Cockpit.js
@@ -1,32 +1,19 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useRef} from "react";
 import styles from "../../Components/Cockpit/Cockpit.css";
 
 const cockpit = (props) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const toggleButtonRef = useRef(null);
 
-/*    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useEffect(() => {
-        console.log("[Cockpit.js] useEffect");
-
-        //Imitate a Http Request
-       const timer = setTimeout(() => {
-            alert("Data saved to cloud!")
-        }, 1000);
-
-        return () => {
-            clearTimeout(timer);
-            console.log("[Cockpit.js] cleanup work in useEffect()");
-        }
-    }, []); //Only triggers once the app is rendered. Add values (like props.persons) inside the array to trigger useEffect() every time a change is made to that value. May take in several values. */
-
-    //You may have more than one useEffect setups, so it can be triggered by different changes!
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useEffect(() => {
-        console.log("[Cockpit.js] 2nd useEffect");
-
+        console.log("[Cockpit.js] useEffect");
+        toggleButtonRef.current.click();
         return () => {
-            console.log("[Cockpit.js] cleanup work in  2nd useEffect()");
-        }
-    });
+            console.log("[Cockpit.js] cleanup work in useEffect()");
+        };
+    }, []); //Only triggers once the app is rendered. Add values (like props.persons) inside the array to trigger useEffect() every time a change is made to that value. May take in several values.
+    //You may have more than one useEffect setups, so it can be triggered by different changes!
 
     const assignedClasses = [];
     let btnClass = "";
@@ -53,6 +40,7 @@ const cockpit = (props) => {
             <h1>{props.title}</h1>
             <p className={assignedClasses.join(' ')}>{counterText}</p>
             <button
+                ref={toggleButtonRef}
                 className={btnClass}
                 onClick={props.clicked}>
                 Toggle Persons
