f212ddde70b545b17f0069050b60ab9143a74587
FredrikPedersen <fredrikhp@gmail.com>
2019-11-04 07:42:38 +0100

    Completed Section 7: Part 91: Component Update Lifecycle (for props Changes)

--- react-complete-guide/src/Components/Persons/Person/Person.js
+++ react-complete-guide/src/Components/Persons/Person/Person.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, {Component} from "react";
 import classes from "./Person.css";
 
-const person = (props) => {
-    console.log("[Person.js rendering...");
-    return (
-        <div className={classes.Person}>
-            <p onClick={props.click}>My name is {props.name}, and I am {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.change} value={props.name}/>
-        </div>
-    );
-};
 
-export default person;
+class Person extends Component {
+    render() {
+        console.log("[Person.js rendering...");
+        return (
+            <div className={classes.Person}>
+                <p onClick={this.props.click}>My name is {this.props.name}, and I am {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.change} value={this.props.name}/>
+            </div>
+        );
+    }
+}
+
+export default Person;
