603bb52c392026822a98cdb5bf78bbaefe546400
FredrikPedersen <fredrikhp@gmail.com>
2019-11-05 09:58:31 +0100

    Completed Section 8: Part 159: Calculating the Ingredient Sum Dynamically

--- burger_app/src/components/Burger/Burger.js
+++ burger_app/src/components/Burger/Burger.js
@@ -3,16 +3,30 @@ import styles from "./Burger.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
-    const tansformedIngredients = Object.keys(props.ingredients).map(igKey => {
+
+    //Initially gives us an array of arrays with the number of each ingredient
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
         return [...Array(props.ingredients[igKey])].map((_, i) => {
             return <BurgerIngredient key={igKey + i} type={igKey}/>
         })
     });
 
+    //Flattens the array so we only have a single array with a length equal to the number of ingredients
+    transformedIngredients = transformedIngredients.reduce((arr, el) => {
+        return arr.concat(el)
+    }, []);
+
+    //Try commenting out the reduce-method to see what the nested arrays look like before getting flattened.
+    console.log(transformedIngredients);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+
     return (
         <div className={styles.burger}>
             <BurgerIngredient type="bread-top"/>
-            {tansformedIngredients}
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
