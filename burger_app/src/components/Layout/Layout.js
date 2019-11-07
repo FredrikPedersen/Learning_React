8a951cc2bf1e6588d82002982ffe97d400b3fd64
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-07 15:06:19 +0100

    Completed Section 8: Part 172: Adding a Toolbar

--- burger_app/src/components/Layout/Layout.js
+++ burger_app/src/components/Layout/Layout.js
@@ -1,9 +1,10 @@
 import React from "react";
 import styles from "./Layout.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const layout = (props) => (
     <>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar/>
         <main className={styles.content}>{props.children}</main>
     </>
 );
