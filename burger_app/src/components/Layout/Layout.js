483c487dd2d38bf9833bc6dbdbcadbf423d4b31e
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-07 15:41:57 +0100

    Completed Section 8: Part 175: Creating a Responsive Sidedrawer

--- burger_app/src/components/Layout/Layout.js
+++ burger_app/src/components/Layout/Layout.js
@@ -1,9 +1,11 @@
 import React from "react";
 import styles from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 const layout = (props) => (
     <>
+        <SideDrawer/>
         <Toolbar/>
         <main className={styles.content}>{props.children}</main>
     </>
