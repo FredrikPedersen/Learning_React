c4b62545367173caf0bd1c9cdad8fd1215e15e40
FredrikPedersen <fredrikhp@gmail.com>
2019-11-09 00:10:01 +0100

    Completed Section 10: Part 210:  Sending a POST Request

--- burger_app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger_app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,7 @@ import Burger from "../../components/Burger/Burger"
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -77,7 +78,27 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-        alert("You continue!");
+        const order =  {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: "Fredrik Pedersen",
+                address: {
+                    street: "Streetway 123",
+                    zipCode: "1234",
+                    country: "Norway"
+                },
+                email: "test@test.com"
+            },
+            deliveryMethod: "Gotta Go Fast"
+        };
+
+        axios.post("/orders.json", order)
+            .then(response => {console.log(response)})
+            .catch(error => {console.log(error)});
+
+        alert("Order recieved! :)");
+        this.purchaseCancelHandler();
     };
 
     render() {
