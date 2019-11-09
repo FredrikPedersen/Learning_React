64296dc4f82a7784e26a8c0bc952a8a62cf56a29
FredrikPedersen <fredrikhp@gmail.com>
2019-11-09 15:40:43 +0100

    Completed Section 11: Part 245: Lazy Loading with React Suspense (16.6) (NB! Added to reachin_out_to_the_web instead of Max's example project!)

--- reaching_out_to_the_web/src/containers/Blog/Blog.js
+++ reaching_out_to_the_web/src/containers/Blog/Blog.js
@@ -1,13 +1,16 @@
-import React, {Component} from "react";
+import React, {Component, Suspense} from "react";
 import './Blog.css';
 import Posts from "./Posts/Posts";
 import asyncComponent from "../../higherOrderComponents/asyncComponent";
 import {Route, NavLink, Switch, Redirect} from "react-router-dom";
 
-//Makes sure the NewPost component is only loaded when needed, as in this enables lazy loading of NewPost.
+/* Enables Lazy loading for NewPost. The asyncComponent-way of doing this is outdated
 const AsyncNewPost = asyncComponent(() => {
     return import("./NewPost/NewPost");
-});
+}); */
+
+//Enables lazy loading for NewPost using React's own lazy-function. Must React Suspense and be rendered as shown below.
+const NewPost = React.lazy(() => import("./NewPost/NewPost"));
 
 class Blog extends Component {
     state = {
@@ -38,10 +41,19 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    {this.state.auth ? <Route path="/new-post" exact component={AsyncNewPost}/> : null} {/*Example on how to use a guard (in combination with the <Redirect from="/" to="/posts" />). Set auth to false to block access */}
+                    {
+                        this.state.auth ?
+                            <Route path="/new-post" exact render={() =>
+                                <Suspense
+                                    fallback={<div>Loading...</div>}>
+                                    <NewPost/>
+                                </Suspense>}/>
+                            : null
+                    } {/*Example on how to use a guard (in combination with the <Redirect from="/" to="/posts" />). Set auth to false to block access */}
                     <Route path="/posts" component={Posts}/>
-                    <Route render={() => <h1>Not Found</h1>}/> {/* This will catch any unknown route and render whatever you want. Will not be triggered if a redirect similar to the one below is active */}
-                    <Redirect from="/" to="/posts" />
+                    <Route render={() => <h1>Not
+                        Found</h1>}/> {/* This will catch any unknown route and render whatever you want. Will not be triggered if a redirect similar to the one below is active */}
+                    <Redirect from="/" to="/posts"/>
                 </Switch>
             </div>
         );
