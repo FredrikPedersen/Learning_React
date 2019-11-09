04526863e82cd6d59c8fe3c85582f5ade4678946
FredrikPedersen <fredrikhp@gmail.com>
2019-11-09 13:15:31 +0100

    Completed Section 10: Part 230:  Styling the Active Route

--- reaching_out_to_the_web/src/containers/Blog/Blog.js
+++ reaching_out_to_the_web/src/containers/Blog/Blog.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 import './Blog.css';
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
-import {Route, Link} from "react-router-dom";
+import {Route, NavLink} from "react-router-dom";
 
 class Blog extends Component {
     render() {
@@ -11,13 +11,20 @@ class Blog extends Component {
                 <header>
                     <nav className="Blog">
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                            <li><NavLink
+                                to="/"
+                                exact
+                                activeClassName="active">Home</NavLink></li>
+                            /*
+                            Setting activeClassName to "active" is redundant. This is just to showcase how to configure the appended CSS class
+                            You may also use activeStyle for inline styling, works just like the normal inline styling. 
+                             */
+                            <li><NavLink to={{
                                 pathname: "/new-post",
                                 //Examples which won't do anything in this app, simply for showcasing the configuration options
                                 hash: "#submit",
                                 search: "?quick-submit=true"
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
