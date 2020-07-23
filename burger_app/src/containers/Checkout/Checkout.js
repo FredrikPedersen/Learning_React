25d72fadcbf8b9bcc62b250a15247903dd34a393
FredrikPedersen <fredrikhp@gmail.com>
2020-07-23 13:14:54 +0200

    Completed Section 17: Part 311: Redirect to Improve UX

--- burger_app/src/containers/Checkout/Checkout.js
+++ burger_app/src/containers/Checkout/Checkout.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
-import {Route} from "react-router-dom";
-import { connect } from "react-redux";
+import {Route, Redirect} from "react-router-dom";
+import {connect} from "react-redux";
 
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
 import ContactData from "./ContactData/ContactData";
@@ -16,16 +16,24 @@ class Checkout extends Component {
     };
 
     render() {
-        return (
-            <div>
-                <CheckoutSummary
-                    ingredients={this.props.ingredients}
-                    checkoutCancelled={this.checkoutCancelledHandler}
-                    checkoutContinued={this.checkoutContinuedHandler}/>
+        let summary = <Redirect to="/"/>;
+
+        if (this.props.ingredients) {
+            summary = (
+                <div>
+                    <CheckoutSummary
+                        ingredients={this.props.ingredients}
+                        checkoutCancelled={this.checkoutCancelledHandler}
+                        checkoutContinued={this.checkoutContinuedHandler}/>
                     <Route
                         path={this.props.match.path + "/contact-data"}
                         component={ContactData}/>
-            </div>
+                </div>
+            )
+        }
+
+        return (
+            {summary}
         );
     }
 }
