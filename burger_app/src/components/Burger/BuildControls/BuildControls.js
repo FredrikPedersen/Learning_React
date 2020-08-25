9540bc6879ad63acf79f5831ecb39e2661771f9e
FredrikPedersen <fredrikhp@gmail.com>
2020-08-25 17:17:58 +0200

    Completed Section 18: Part 334: Forwarding Unauthenticated Users

--- burger_app/src/components/Burger/BuildControls/BuildControls.js
+++ burger_app/src/components/Burger/BuildControls/BuildControls.js
@@ -24,7 +24,7 @@ const buildControls = (props) => (
             className={styles.OrderButton}
             disabled={!props.purchasable}
             onClick={props.ordered}>
-            ORDER NOW</button>
+            {props.isAuth ? "ORDER NOW" : "Sign In To Order"}</button>
     </div>
 );
 
--- burger_app/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger_app/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -30,7 +30,11 @@ class BurgerBuilder extends Component {
     };
 
     purchaseHandler = () => {
-        this.setState({purchasing: true});
+        if (this.props.isAuthenticated) {
+            this.setState({purchasing: true});
+        } else {
+            this.props.history.push("/auth")
+        }
     };
 
     purchaseCancelHandler = () => {
@@ -67,7 +71,8 @@ class BurgerBuilder extends Component {
                         disabled={disabledInfo}
                         purchasable={this.updatePurchaseState(this.props.ingredients)}
                         ordered={this.purchaseHandler}
-                        price={this.props.price}/>
+                        price={this.props.price}
+                        isAuth={this.props.isAuthenticated}/>
                 </>
             );
 
@@ -93,7 +98,8 @@ const mapStateToProps = state => {
     return {
         ingredients: state.burgerBuilder.ingredients,
         price: state.burgerBuilder.totalPrice,
-        error: state.burgerBuilder.error
+        error: state.burgerBuilder.error,
+        isAuthenticated: state.auth.token !== null
     }
 };
 
