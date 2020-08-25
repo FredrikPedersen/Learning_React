550ba55d543c53f268799ed8c5f31d4bd24c4176
FredrikPedersen <fredrikhp@gmail.com>
2020-08-25 19:40:20 +0200

    Completed Section 18: Part 339: Guarding Routes

--- burger_app/src/App.js
+++ burger_app/src/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import {Route, Switch, withRouter } from "react-router-dom";
-import { connect} from "react-redux";
+import {Route, Switch, withRouter, Redirect} from "react-router-dom";
+import {connect} from "react-redux";
 
 import Layout from "./containers/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
@@ -16,21 +16,41 @@ class App extends Component {
     }
 
     render() {
+        let routes = (
+            <Switch>
+                <Route path="/auth" component={Auth}/>
+                <Route path="/" exact component={BurgerBuilder}/>
+                <Redirect to="/"/>
+            </Switch>
+        );
+
+        if (this.props.isAuthenticated) {
+            routes = (
+                <Switch>
+                    <Route path="/checkout" component={Checkout}/>
+                    <Route path="/orders" component={Orders}/>
+                    <Route path="/auth" component={Auth}/>
+                    <Route path="/logout" component={Logout}/>
+                    <Route path="/" exact component={BurgerBuilder}/>
+                </Switch>
+            );
+        }
+
         return (
             <div>
                 <Layout>
-                    <Switch>
-                        <Route path="/checkout" component={Checkout}/>
-                        <Route path="/orders" component={Orders}/>
-                        <Route path="/auth" component={Auth}/>
-                        <Route path="/logout" component={Logout}/>
-                        <Route path="/" exact component={BurgerBuilder}/>
-                    </Switch>
+                    {routes}
                 </Layout>
             </div>
         );
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    }
+};
 
 const mapDispatchToProps = dispatch => {
     return {
