399b7937c5e79fa8e136a1c5c8e15d65e2be918d
FredrikPedersen <fredrikhp@gmail.com>
2020-11-04 21:31:50 +0100

    Completed Section 26: Part 435: useEffect() & Loading Data

--- react_hooks/src/components/Ingredients/Ingredients.js
+++ react_hooks/src/components/Ingredients/Ingredients.js
@@ -1,7 +1,7 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 
-import {DATABASE_URL} from "../constants";
+import {INGREDIENTS_URL} from "../constants";
 import IngredientForm from "./IngredientForm";
 import IngredientList from "./IngredientList";
 import Search from "./Search";
@@ -9,8 +9,22 @@ import Search from "./Search";
 const Ingredients = () => {
     const [ingredientsState, setIngredientsState] = useState([]);
 
+    useEffect(() => {
+        axios.get(INGREDIENTS_URL).then(response => {
+            const loadedIngredients = [];
+            for (const key in response.data) {
+                loadedIngredients.push({
+                    id: key,
+                    title: response.data[key].ingredient.title,
+                    amount: response.data[key].ingredient.amount
+                });
+            }
+            setIngredientsState(loadedIngredients);
+        });
+    }, []);
+
     const addIngredientHandler = (ingredient) => {
-        axios.post(DATABASE_URL + "ingredients.json", JSON.stringify({ingredient})).then(response => {
+        axios.post(INGREDIENTS_URL, JSON.stringify({ingredient})).then(response => {
             setIngredientsState(prevIngredients => [
                 ...prevIngredients,
                 {id: response.data.name, ...ingredient}
--- react_hooks/src/components/constants.js
+++ react_hooks/src/components/constants.js
@@ -1,1 +1,2 @@
 export const DATABASE_URL = process.env.REACT_APP_DATABASE_URL;
+export const INGREDIENTS_URL = DATABASE_URL + "ingredients.json";
