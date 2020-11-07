41356e84ffe5015b994d5b6b704b89206afa3733
FredrikPedersen <fredrikhp@gmail.com>
2020-11-07 14:45:21 +0100

    Completed Section 26: Part 439: Working with Refs & useRef()

--- react_hooks/src/components/Ingredients/Search.js
+++ react_hooks/src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, {useState, useEffect, useRef} from "react";
 import axios from "axios";
 
 import {INGREDIENTS_URL} from "../constants";
@@ -7,36 +7,46 @@ import "./Search.css";
 
 const Search = React.memo(props => {
     const {onLoadIngredients} = props;
-
     const [enteredFilterState, setEnteredFilterState] = useState("");
+    const inputRef = useRef();
 
     useEffect(() => {
-        const query =  enteredFilterState.length === 0 ? "" : `?orderBy="title"&equalTo="${enteredFilterState}"`;
+        setTimeout(() => {
+            if (enteredFilterState === inputRef.current.value) {
+                const query = enteredFilterState.length === 0 ? "" : `?orderBy="title"&equalTo="${enteredFilterState}"`;
 
-        axios.get(INGREDIENTS_URL + query).then(response => {
-            const loadedIngredients = [];
+                axios.get(INGREDIENTS_URL + query).then(response => {
+                    const loadedIngredients = [];
 
-            for (const key in response.data) {
-                loadedIngredients.push({
-                    id: key,
-                    title: response.data[key].ingredient.title,
-                    amount: response.data[key].ingredient.amount
+                    for (const key in response.data) {
+                        loadedIngredients.push({
+                            id: key,
+                            title: response.data[key].ingredient.title,
+                            amount: response.data[key].ingredient.amount
+                        });
+                    }
+                    onLoadIngredients(loadedIngredients);
                 });
             }
-            onLoadIngredients(loadedIngredients);
-        });
-    }, [enteredFilterState, onLoadIngredients]);
+        }, 500);
+    }, [enteredFilterState, onLoadIngredients, inputRef]);
 
-  return (
-    <section className="search">
-      <Card>
-        <div className="search-input">
-          <label>Filter by Title</label>
-          <input type="text" value={enteredFilterState} onChange= {event => {setEnteredFilterState(event.target.value)}}/>
-        </div>
-      </Card>
-    </section>
-  );
+
+    return (
+        <section className="search">
+            <Card>
+                <div className="search-input">
+                    <label>Filter by Title</label>
+                    <input
+                        ref={inputRef}
+                        type="text"
+                        value={enteredFilterState} onChange={event => {
+                        setEnteredFilterState(event.target.value)
+                    }}/>
+                </div>
+            </Card>
+        </section>
+    );
 });
 
 export default Search;
