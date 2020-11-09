b8d87875cacc15002c6a448a579ece70223f0216
Fredrik Pedersen <fredrikhp@gmail.com>
2020-11-09 21:21:17 +0000

    Completed Section 26: Part 444: Understanding useReducer()

--- react_hooks/src/components/Ingredients/Ingredients.js
+++ react_hooks/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, {useState, useCallback} from "react";
+import React, {useReducer, useState, useCallback} from "react";
 import axios from "axios";
 
 import {INGREDIENTS_URL, DELETE_INGREDIENTS_URL} from "../constants";
@@ -7,21 +7,35 @@ import IngredientList from "./IngredientList";
 import ErrorModal from "../UI/ErrorModal";
 import Search from "./Search";
 
+const ingredientReducer = (currentState, action) => {
+    switch (action.type) {
+        case "SET":
+            return action.ingredients;
+        case "ADD":
+            return [...currentState, action.ingredient]
+        case "DELETE":
+            return currentState.filter(ingredient => ingredient.id !== action.id);
+        default:
+            throw new Error("SHOULD NOT GET HERE!");
+    }
+};
+
 const Ingredients = () => {
-    const [ingredientsState, setIngredientsState] = useState([]);
+    const [ingredientsState, dispatch] = useReducer(ingredientReducer, []);
+
     const [isLoadingState, setIsLoadingState] = useState(false);
     const [errorState, setErrorState] = useState();
 
     //Use callback caches the function so it is not re-created when the component re-renders.
     const filteredIngredientsHandler = useCallback(filteredIngredients => {
-        setIngredientsState(filteredIngredients);
+        dispatch({type: "SET", ingredients: filteredIngredients});
     }, []);
 
     const addIngredientHandler = (ingredient) => {
         setIsLoadingState(true);
         axios.post(INGREDIENTS_URL, JSON.stringify({ingredient})).then(response => {
             setIsLoadingState(false);
-            setIngredientsState(prevIngredients => [...prevIngredients, {id: response.data.name, ...ingredient}]);
+            dispatch({type: "ADD", ingredient: {id: response.data.name, ...ingredient}})
         }).catch(error => {
             setErrorState("Something went wrong when adding an ingredient");
         });
@@ -32,9 +46,7 @@ const Ingredients = () => {
         setIsLoadingState(true);
         axios.delete(DELETE_INGREDIENTS_URL(ingredientId)).then(response => {
             setIsLoadingState(false);
-            setIngredientsState(prevIngredients =>
-                prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
-            );
+            dispatch({type: "DELETE", id: ingredientId})
         }).catch(error => {
             setErrorState("Something went wrong when deleting an ingredient");
         });
