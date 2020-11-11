215a88a6fe3f75f38f16732c80169e7e99ed3beb
Fredrik Pedersen <fredrikhp@gmail.com>
2020-11-11 10:09:42 +0100

    Completed Section 27: Part 453: Converting <App>

--- burger_app/src/App.js
+++ burger_app/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useEffect} from 'react';
 import {Route, Switch, withRouter, Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import asyncComponent from "./hoc/asyncComponent/asyncComponent";
@@ -20,41 +20,41 @@ const asyncAuth = asyncComponent(() => {
     return import("./containers/Auth/Auth")
 });
 
-class App extends Component {
-    componentDidMount() {
-        this.props.onTryAutoSignup();
-    }
+const App = (props) => {
 
-    render() {
-        let routes = (
+    //Pass no dependencies to useEffect to ensure it only runs once when the component is mounted
+    useEffect(() => {
+        props.onTryAutoSignup();
+    }, [])
+
+    let routes = (
+        <Switch>
+            <Route path="/auth" component={asyncAuth}/>
+            <Route path="/" exact component={BurgerBuilder}/>
+            <Redirect to="/"/>
+        </Switch>
+    );
+
+    if (props.isAuthenticated) {
+        routes = (
             <Switch>
+                <Route path="/checkout" component={asyncCheckout}/>
+                <Route path="/orders" component={asyncOrders}/>
+                <Route path="/logout" component={Logout}/>
                 <Route path="/auth" component={asyncAuth}/>
                 <Route path="/" exact component={BurgerBuilder}/>
                 <Redirect to="/"/>
             </Switch>
         );
-
-        if (this.props.isAuthenticated) {
-            routes = (
-                <Switch>
-                    <Route path="/checkout" component={asyncCheckout}/>
-                    <Route path="/orders" component={asyncOrders}/>
-                    <Route path="/logout" component={Logout}/>
-                    <Route path="/auth" component={asyncAuth}/>
-                    <Route path="/" exact component={BurgerBuilder}/>
-                    <Redirect to="/"/>
-                </Switch>
-            );
-        }
-
-        return (
-            <div>
-                <Layout>
-                    {routes}
-                </Layout>
-            </div>
-        );
     }
+
+    return (
+        <div>
+            <Layout>
+                {routes}
+            </Layout>
+        </div>
+    );
 }
 
 const mapStateToProps = state => {
