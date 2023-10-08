a52bd6abbc770ab8db065475eb4950b1abf81c89
Fredrik Pedersen <fredrikhp@gmail.com>
2023-10-08 11:04:22 +0200

    Completed Section 3: Part 61: Extracting Dunamic Path Segment Data (Dynamic Routes)

--- nextjs_complete_guide/pages_and_file_based_routing/pages/portfolio/[projectid].js
+++ nextjs_complete_guide/pages_and_file_based_routing/pages/portfolio/[projectid].js
@@ -1,4 +1,11 @@
+import { useRouter } from 'next/router'
+
 export default function PortfolioProjectPage() {
+    const router = useRouter()
+
+    console.log(router.pathname) // /portfolio/[projectId]
+    console.log(router.query) // going to localhost:3000/portfolio/1 -> {projectId: "1"}
+
     return (
         <>
             <h1>The Portfolio Project Page</h1>
