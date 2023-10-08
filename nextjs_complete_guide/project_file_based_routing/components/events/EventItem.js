b57362d2716d025604fb8960a1ba5a3f7bf281b7
Fredrik Pedersen <fredrikhp@gmail.com>
2023-10-08 18:05:27 +0200

    Completed Section 4: Part 76: Adding More Components & Connecting Components

--- nextjs_complete_guide/project_file_based_routing/components/events/EventItem.js
+++ nextjs_complete_guide/project_file_based_routing/components/events/EventItem.js
@@ -1,5 +1,45 @@
+import Link from "next/link";
+import {useMemo} from "react";
+
 export default function EventItem(props) {
+    const {title, image, date, location, id} = props
+
+    const formattedDate = useMemo(() => {
+        return new Date(date).toLocaleDateString("nb-no", {
+            day: "numeric",
+            month: "long",
+            year: 'numeric'
+        })
+    }, [date])
+
+    const formattedAddress = useMemo(() => {
+        return location.replace(", ", "\n")
+    }, [location])
+
+    const exploreLink = useMemo(() => {
+        return {
+            pathname: "/events/[eventId]",
+            query: {eventId: id}
+        }
+    }, [id])
+
     return (
-        <li></li>
+        <li>
+            <img src={"/" + image} alt={title}/>
+            <div>
+                <div>
+                    <h2>{title}</h2>
+                    <div>
+                        <time>{formattedDate}</time>
+                    </div>
+                    <div>
+                        <address>{formattedAddress}</address>
+                    </div>
+                </div>
+                <div>
+                    <Link href={exploreLink}>Explore Event</Link>
+                </div>
+            </div>
+        </li>
     )
 }
--- nextjs_complete_guide/project_file_based_routing/pages/index.js
+++ nextjs_complete_guide/project_file_based_routing/pages/index.js
@@ -1,11 +1,12 @@
 import {getFeaturedEvents} from "../data/dummyData";
+import EventList from "../components/events/EventList";
 
 export default function HomePage() {
     const featuredEvents = getFeaturedEvents()
 
     return (
         <>
-
+            <EventList items={featuredEvents}/>
         </>
     )
 }
