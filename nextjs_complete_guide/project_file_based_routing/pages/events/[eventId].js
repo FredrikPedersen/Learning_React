36554935ef173543b7b33742ea6b59eef3bd7679
Fredrik Pedersen <fredrikhp@gmail.com>
2023-10-29 16:14:41 +0100

    Completed Section 4: Part 80: Adding the "Event Detail" Page (Dynamic Route)

--- nextjs_complete_guide/project_file_based_routing/pages/events/[eventId].js
+++ nextjs_complete_guide/project_file_based_routing/pages/events/[eventId].js
@@ -1,7 +1,37 @@
+import {useRouter} from "next/router";
+import {getEventById} from "../../data/dummyData";
+import {useMemo} from "react";
+import EventSummary from "../../components/eventDetail/EventSummary";
+import EventLogistics from "../../components/eventDetail/EventLogistics";
+import EventContent from "../../components/eventDetail/EventContent";
+
 export default function EventDetailPage() {
+    const router = useRouter()
+
+    const event = useMemo(() => {
+        const eventId = router.query.eventId
+        const event = getEventById(eventId)
+
+        if (!event) {
+            return <p>No event found!</p>
+        }
+
+        return event
+    }, [router.query]);
+
+
     return (
         <>
-            <h1>Single Event Page</h1>
+            <EventSummary title={event.title}/>
+            <EventLogistics
+                date={event.date}
+                address={event.location}
+                image={event.image}
+                imageAlt={event.title}
+            />
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
         </>
     )
 }
