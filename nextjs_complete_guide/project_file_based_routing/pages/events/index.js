3d828f1a9e00222ca9d669edbc7787bf31b1a5b8
Fredrik Pedersen <fredrikhp@gmail.com>
2023-10-29 16:41:00 +0100

    Completed Section 4: Part 83: Adding a Filter From for Filtering Events

--- nextjs_complete_guide/project_file_based_routing/pages/events/index.js
+++ nextjs_complete_guide/project_file_based_routing/pages/events/index.js
@@ -1,11 +1,13 @@
 import {getAllEvents} from "../../data/dummyData";
 import EventList from "../../components/events/EventList";
+import EventsSearch from "../../components/events/EventsSearch";
 
 export default function AllEventsPage() {
     const events = getAllEvents()
 
     return (
         <>
+            <EventsSearch/>
             <EventList items={events}/>
         </>
     )
