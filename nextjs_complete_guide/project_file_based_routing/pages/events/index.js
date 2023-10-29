b424bf73ba02c6d6cd5b5298d001438867820844
Fredrik Pedersen <fredrikhp@gmail.com>
2023-10-29 16:54:34 +0100

    Completed Section 4: Part 84: Navigating to the "Filtered Events" Page Programmatically

--- nextjs_complete_guide/project_file_based_routing/pages/events/index.js
+++ nextjs_complete_guide/project_file_based_routing/pages/events/index.js
@@ -1,13 +1,22 @@
 import {getAllEvents} from "../../data/dummyData";
 import EventList from "../../components/events/EventList";
 import EventsSearch from "../../components/events/EventsSearch";
+import {useCallback} from "react";
+import {useRouter} from "next/router";
 
 export default function AllEventsPage() {
     const events = getAllEvents()
+    const router = useRouter()
+
+    const findEventsHandler = useCallback((year, month) => {
+        const fullPath = `/events/${year}/${month}`
+
+        router.push(fullPath)
+    }, [])
 
     return (
         <>
-            <EventsSearch/>
+            <EventsSearch onSearch={findEventsHandler}/>
             <EventList items={events}/>
         </>
     )
