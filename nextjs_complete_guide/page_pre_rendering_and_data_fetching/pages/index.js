9ce1b75854d1f28d44fa3ef8403d8ffd405d51d6
Fredrik Pedersen <fredrikhp@gmail.com>
2023-12-10 08:25:30 +0100

    Completed Section 6: Part 175: Adding getStaticProps to Pages (NB! Course was updated)

--- nextjs_complete_guide/page_pre_rendering_and_data_fetching/pages/index.js
+++ nextjs_complete_guide/page_pre_rendering_and_data_fetching/pages/index.js
@@ -1,11 +1,22 @@
-function HomePage(props) {
-  return (
-    <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
-    </ul>
-  );
+export default function HomePage(props) {
+    const {products} = props
+
+    return (
+        <ul>
+            {products.map(product => (
+                <li key={product.id}>{product.title}</li>
+            ))}
+        </ul>
+    )
 }
 
-export default HomePage;
+export async function getStaticProps() {
+    return {
+        props: {
+            products: [{
+                id: 'p1',
+                title: 'Product 1'
+            }]
+        }
+    }
+}
